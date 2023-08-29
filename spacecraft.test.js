5281bc50bd0ddf9ee2250685c0205f78eec22507
Zeel-1111 <124433079+Zeel-1111@users.noreply.github.com>
2023-08-30 01:45:17 +0530

    Update spacecraft.test.js

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -16,4 +16,24 @@ describe('Spacecraft Control', () => {
         spacecraft.moveBackward();
         expect(spacecraft.getPosition()).toEqual({ x: 0, y: -1, z: 0 });
     });
+    //for instruction 'l'
+    test('Turn Left', () => {
+        spacecraft.turnLeft();
+        expect(spacecraft.getDirection()).toBe('W');
+    });
+    //for instruction 'r'
+    test('Turn Right', () => {
+        spacecraft.turnRight();
+        expect(spacecraft.getDirection()).toBe('E');
+    });
+    //for instruction 'u'
+    test('Turn Up', () => {
+        spacecraft.turnUp();
+        expect(spacecraft.getDirection()).toBe('U');
+    });
+    //for instruction 'd'
+    test('Turn Down', () => {
+        spacecraft.turnDown();
+        expect(spacecraft.getDirection()).toBe('D');
+    });
 }
