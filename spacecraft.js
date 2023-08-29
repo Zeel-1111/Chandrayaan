457b1d79e7b8a707783800fd131acbe10749e42a
Zeel-1111 <124433079+Zeel-1111@users.noreply.github.com>
2023-08-30 01:21:04 +0530

    Update spacecraft.js

--- spacecraft.js
+++ spacecraft.js
@@ -1,1 +1,10 @@
-
+class Spacecraft {
+    //Constructor for 
+    constructor(x, y, z, direction,prev_direction) {
+        this.x = x; 
+        this.y = y;
+        this.z = z;
+        this.direction = direction;  //current direction
+        this.prev_direction = prev_direction;  // Direction before facing  upwards or downwards 
+    }
+}
