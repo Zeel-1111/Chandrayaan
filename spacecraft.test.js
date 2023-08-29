5ba73592dc979c686dd43b23883a573ce2b41eaa
Zeel-1111 <124433079+Zeel-1111@users.noreply.github.com>
2023-08-30 01:17:58 +0530

    Create spacecraft.test.js

---
+++ spacecraft.test.js
@@ -0,0 +1,9 @@
+const Spacecraft = require('./spacecraft');
+
+describe('Spacecraft Control', () => {
+    let spacecraft;
+
+    beforeEach(() => {
+        spacecraft = new Spacecraft(0, 0, 0, 'N');
+    });
+}
