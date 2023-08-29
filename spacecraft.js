0d76eec2326993d4ce5dcb6d6b9a95e2633b7f3a
Zeel-1111 <124433079+Zeel-1111@users.noreply.github.com>
2023-08-30 01:44:30 +0530

    Update spacecraft.js

--- spacecraft.js
+++ spacecraft.js
@@ -7,4 +7,66 @@ class Spacecraft {
         this.direction = direction;  //current direction
         this.prev_direction = prev_direction;  // Direction before facing  upwards or downwards 
     }
+    moveForward() {
+        // logic to move forward
+        switch (this.direction) {
+            case 'N':
+                this.y++;
+                console.log("North",this.y);
+                break;
+            case 'S':
+                this.y--;
+                console.log("South",this.y);
+                break;
+            case 'E':
+                this.x++;
+                console.log("East",this.x);
+                break;
+            case 'W':
+                this.x--;
+                console.log("West",this.x);
+                break;
+            case 'U':
+                this.z++;
+                console.log("Up",this.z);
+                break;
+            case 'D':
+                this.z--;
+                console.log("Down",this.z);
+                break;
+        }
+    }
+
+    moveBackward() {
+        //logic to move backward
+        switch (this.direction) {
+            case 'N':
+                this.y--;
+                console.log("North",this.y);
+                break;
+            case 'S':
+                this.y++;
+                console.log("South",this.y);
+                break;
+            case 'E':
+                this.x--;
+                console.log("East",this.x);
+                break;
+            case 'W':
+                this.x++;
+                console.log("West",this.x);
+                break;
+            case 'U':
+                this.z--;
+                console.log("Up",this.z);
+                break;
+            case 'D':
+                this.z++;
+                console.log("Down",this.z);
+                break;
+        }
+    }
+
+    
+
 }
